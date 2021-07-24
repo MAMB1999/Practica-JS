00461c8e4a7f64d4cc154b51ff42b0d355e552c7
MAMB1999 <74522304+MAMB1999@users.noreply.github.com>
2021-07-23 23:06:42 -0300

    Inicio de Archivos en 0

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+var palabra = "Buenas Buenaas";
+var prueba = 5;
+var prueba2 = 9 ;
+function textos(){
+document.getElementById("texto").innerHTML = palabra + prueba;
+}
+
+
+var resultado = prueba + prueba2;
+
+document.write(textos())
