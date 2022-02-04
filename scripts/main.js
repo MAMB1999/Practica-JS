fe91177eee0967492614762f4c2fa7a36c6f7112
MAMB1999 <74522304+MAMB1999@users.noreply.github.com>
2022-02-04 11:52:32 -0300

    actualizacion de datos 4/2/2022

--- scripts/main.js
+++ scripts/main.js
@@ -54,6 +54,146 @@
 // "<br> Usando slice() method: " + slicemth + 
 // "<br> Usando substring() method: " + substringmth);
 
+//MANEJO DE ERRORES JAVASCRIP 
+
+// try{
+//     let variable = "f";
+//     if(isNaN(variable)){
+//         throw new Error("El caracter introducido no es un numero");
+// }
+// }catch(error){
+//     console.log("Se produjo un error de tipo:" + error);
+
+// }finally{
+// console.log("llegue al finally");
+
+// // DESTRUCTURACION JAVASCRIP
+// //Es una nueva forma de asignar valores a arreglos y objetos
+// const numeros = [1,2,3];
+// //SIN DESTRUCTURACION
+// let uno = numeros[0],
+//     dos = numeros[1],
+//     tres = numeros[2];
+// document.write(uno,dos,tres);
+// //CON OBJETOS 
+// let persona = {
+//     name:"Moises",
+//     apellido:"montiel",
+//     edad: 22
+// }
+// //CON DESTRUCTURACION
+// const [one,two,three] = numeros;
+// document.write(one,two,three);
+// //CON OBJETOS
+// //LAS VARIABLES DEBEN LLAMARSE IGUAL QUE LOS COMPONENTES DEL OBJETO
+// let[nombre,apellido,edad] = persona;
+// document.write(nombre,apellido,edad);
+
+
+//OBJETOS LITERALES
+// UNA NUEVA FORMA DE ESCRIBIR ATRIBUTOS Y METODOS 
+
+// /*
+// *Las variables pueden inicializarse en la misma linea.
+// *Si el nombre de la variable declarada fuera del objeto es el mismo de la declarada en el objeto se
+// escribe solo el nombre.
+// *los metodos del objeto ahora se escriben: nombreFuncion(){CuerpoDeLafuncion} 
+//  */
+// let name = "Moises", age = 22;
+// let persona = {
+//         name,
+//         apellido:"montiel",
+//         edad,
+//         saludar(){document.write("hola a todos")}
+// }
+
+// //PARAMETOS REST Y OPERADOR SPREAD
+// //Es una manera de agregar parametros a una funcion para evitar el exceso de codigo
+// function parametroRest(a,b,...c){
+//         let resultado = a + b;
+//         c.forEach(function(b){  //con esta funcion podemos agregar a la variable resultado
+//                 resultado += b; //cada parametro extra que pueda recibir la funcion mediante (...c)
+//         });
+//         console.log(resultado); 
+// }
+// parametroRest(1,2,3); // es igual a 6
+// parametroRest(1,2,3,4); // es igual a 10
+
+// //Con el SPREAD operator podemos concatenar arreglos sin que terminen separados entre la data
+// arr1 = [1,2,3,4,5,6];
+// arr2 = [7,8,9,10];
+
+// console.log(arr3 = [arr1,arr2])   //sin spread operator = [[1,2,3,4,5,6],[7,8,9,10]]
+
+// console.log(arr3 = [...arr1,...arr2]) //con spread operator = [1,2,3,4,5,6,7,8,9,10]
+
+// //ARROW FUNCTION
+
+// //funcion anonima
+// const hola = function(){
+//         console.log("hola soy anonima")// al no ser declarada si la funcion se llama antes que la variable resultara error  
+// }
+// //arrow funcion
+// const holaArrow = () => console.log("hola soy una arrow function")
+// //las arrow functions capturan el objeto this en el del scope global
+// const perro ={
+//         nombre:"kenai",
+//         ladrar:() =>{
+//                 console.log(this);
+//         }
+// }
+// perro.ladrar();
+
+// //cuando no usamos funciones arrow el this se queda en el scope del objeto
+// const gato ={
+//         nombre:"pichi",
+//         maullar(){
+//                 console.log(this)
+//         }
+// }
+
+// gato.maullar();
+
+
+// //POO AND PROTOTYPE
+
+// //creamos la clase animal
+// function Animal(Nombre,sexo){
+//         this.Nombre = Nombre;
+//         this.sexo = sexo;
+// }
+// //creamos un perro desde animal
+// let perro = new Animal("Coraje","Hombre");
+// //le añadimos la funcion maullar 
+// Animal.prototype.Maullar = function(){
+//         console.log(`Mi nombre es ${this.Nombre} y soy ${this.sexo}`)     
+// }
+// //creamos la clase gato y seleccionamos las variables
+// function Gato(Nombre,sexo,tamanio){
+//         this.super = Animal;
+//         this.super(Nombre,sexo);
+//         this.tamanio = tamanio;
+// }
+// //Aca creamos Extraemos los datos de animal
+// Gato.prototype = new Animal();
+// //Aca insertamos al prototype de Gato
+// Gato.prototype.constructor = Gato;
+// //Aca reescribimos el metodo Maullar
+// Gato.prototype.Maullar = function() {
+//         console.log("Soy un gato y hago MIAU")
+// }
+// //Aca gato tiene un metodo que no tiene animal
+// Gato.prototype.MaullaMichi = function() {
+//         console.log(`Tambien soy ${this.tamanio}`);
+// }
+// var michi = new Gato("Michi","macho","pequeño");
+// console.log(perro);
+// console.log(michi);
+// perro.Maullar();
+// michi.Maullar();
+// michi.MaullaMichi(); 
+
+//CLASE Y HERENCIA
 
 // //AMBITO GLOBAL Y A LVL DE Function
 
@@ -184,24 +324,25 @@
 // // the onLine property returns a boolean indicanding whether the browser is working online
 // document.write(`Using onLine property: ${navigator.onLine}<br>`);
 
-//About Cookies -----------------------
+// //About Cookies -----------------------
 
-//To create a cookie we use the document.cookie property
-document.cookie = "nombre=Moises";
+// //To create a cookie we use the document.cookie property
+// document.cookie = "nombre=Moises";
 
-//to create multiple cookies, separate the nombre=Moises pairs with a semicolon (;)
-document.cookie = "firstName = Moises2 ; lastName = Roberto";
+// //to create multiple cookies, separate the nombre=Moises pairs with a semicolon (;)
+// document.cookie = "firstName = Moises2 ; lastName = Roberto";
 
-//Add an expiry date (in UTC) so that it does no get deleted when the browser is closed
-document.cookie = "lastName = Roberto1; expires= Mon, 21 Fre 2022 10:00:00 UTC";
+// //Add an expiry date (in UTC) so that it does no get deleted when the browser is closed
+// document.cookie = "lastName = Roberto1; expires= Mon, 21 Fre 2022 10:00:00 UTC";
 
-//getting a cookie value
-//the document.cookie returns a string that contains all the cookies 
-var cookies = document.cookie;
-document.write(cookies,"<br>");
-//to delete a cookie, set the its name to a empty value and use max-age=0
-document.cookie = "firstName=; max-age=0";
-document.write(cookies);
+// //getting a cookie value
+// //the document.cookie returns a string that contains all the cookies 
+// var cookies = document.cookie;
+// document.write(cookies,"<br>");
+// //to delete a cookie, set the its name to a empty value and use max-age=0
+// document.cookie = "firstName=; max-age=0";
+// document.write(cookies);
+
 
 
 
